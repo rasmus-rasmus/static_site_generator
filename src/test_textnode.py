66ced3f105150a122fbdb0d15a562383d186c946
Rasmus Stavenuiter <rkst@rd8.tech>
2024-03-15 23:30:43 +0100

    text to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,15 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_link,
+    text_type_image,
+    text_node_to_html_node
+)
 
 
 class TestTextNode(unittest.TestCase):
@@ -33,6 +42,49 @@ class TestTextNode(unittest.TestCase):
         text_node_1 = TextNode("Test", "bold", "http://example.com")
         text_node_2 = TextNode("Test", "bold")
         self.assertNotEqual(text_node_1, text_node_2)
+        
+        
+    def test_text_node_to_html(self):
+        text_node = TextNode(text="test", text_type=text_type_text)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, text_node.text)
+        
+    def test_text_node_to_html_bold(self):
+        text_node = TextNode(text="test", text_type=text_type_bold)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, text_node.text)
+        
+    def test_text_node_to_html_italic(self):
+        text_node = TextNode(text="test", text_type=text_type_italic)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, text_node.text)
+        
+    def test_text_node_to_html_code(self):
+        text_node = TextNode(text="test", text_type=text_type_code)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, text_node.text)
+        
+    def test_text_node_to_html_link(self):
+        text_node = TextNode(text="test", text_type=text_type_link, url="http://ur.l")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, text_node.text)
+        self.assertDictEqual(html_node.props, {"href": text_node.url})
+        
+    def test_text_node_to_html_image(self):
+        text_node = TextNode(text="test", text_type=text_type_image, url="http://ur.l")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertDictEqual(html_node.props, {"src": text_node.url, "alt": text_node.text})
+        
+    def test_text_node_to_html_unknown_type(self):
+        text_node = TextNode(text="test", text_type="unknown")
+        self.assertRaises(ValueError, text_node_to_html_node, text_node)
         
         
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,14 @@
+from htmlnode import LeafNode
+
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -9,3 +20,19 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text!r}, {self.text_type!r}, {self.url!r})"
+    
+    
+def text_node_to_html_node(text_node):
+        if text_node.text_type == text_type_text:
+            return LeafNode(value=text_node.text)
+        if text_node.text_type == text_type_bold:
+            return LeafNode(tag="b", value=text_node.text)
+        if text_node.text_type == text_type_italic:
+            return LeafNode(tag="i", value=text_node.text)
+        if text_node.text_type == text_type_code:
+            return LeafNode(tag="code", value=text_node.text)
+        if text_node.text_type == text_type_link:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        if text_node.text_type == text_type_image:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+        raise ValueError(f"Unknown text type: {text_node.text_type}")
