2e8da6afabe04a3af634e2909e377ea4f08e3be9
Rasmus Stavenuiter <rkst@rd8.tech>
2024-03-29 13:37:31 +0100

    block to block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -2,6 +2,54 @@ from textnode import *
 
 from re import split as resplit
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ul= "unordered list"
+block_type_ol = "ordered list"
+
 def markdown_to_blocks(markdown):
     markdown = "".join(resplit(r" {2,}", markdown)) # Remove excessive internal whitespaces
     return [block.strip() for block in filter(lambda a: len(a) > 0, resplit(r"\n{2,}", markdown))]
+
+
+def block_to_block_type(block):
+    # Heading
+    if block[0] == "#":
+        index = 1
+        while block[index] == "#":
+            index += 1
+        if index < 5 and block[index] == " ":
+            return block_type_heading
+        else:
+            return block_type_paragraph
+    
+    # code block
+    if block[0:3] == "```" and block[-3:] == "```":
+        return block_type_code
+    
+    lines = block.split("\n")
+    # ordered list
+    if lines[0][0] == "1" and lines[0][1] == "." and lines[0][2] == " ":
+        for index in range(1, len(lines)):
+            if lines[index][0] != str(index+1) or lines[index][1] != "." or lines[index][2] != " ":
+                return block_type_paragraph
+        return block_type_ol
+    
+    # unordered list
+    if (lines[0][0] == "*" or lines[0][0] == "-") and lines[0][1] == " ":
+        for index in range(1, len(lines)):
+            if (lines[index][0] != "*" and lines[index][0] != "-") or lines[index][1] != " ":
+                return block_type_paragraph
+        return block_type_ul
+    
+    # quote
+    if lines[0][0] == ">":
+        for index in range(1, len(lines)):
+            if lines[index][0] != ">":
+                return block_type_paragraph
+        return block_type_quote
+    
+    # paragraph
+    return block_type_paragraph
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import *
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -29,3 +29,58 @@ class TestMarkdownToBlocks(unittest.TestCase):
         blocks = markdown_to_blocks(markdown)
         
         self.assertListEqual(blocks, ["This is a block", "and this is another"])
+        
+        
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading(self):
+        heading_1 = "# This is a heading"
+        heading_2 = "### And this is too"
+        non_heading = "####### And this is not"
+        self.assertEqual(block_to_block_type(heading_1), block_type_heading)
+        self.assertEqual(block_to_block_type(heading_2), block_type_heading)
+        self.assertEqual(block_to_block_type(non_heading), block_type_paragraph)
+        
+    def test_code(self):
+        code = "```This is code```"
+        non_code = "This ```is``` a paragraph"
+        self.assertEqual(block_to_block_type(code), block_type_code)
+        self.assertEqual(block_to_block_type(non_code), block_type_paragraph)
+        
+    def test_quote(self):
+        quote = ">This is a quote\n>spanning multiple lines\n> some of which start with a with a whitespace"
+        self.assertEqual(block_to_block_type(quote), block_type_quote)
+        
+    def test_quote_missing_character(self):
+        non_quote = ">This is a quote\n>spanning multiple lines\nbut the last line is missing the '>' character"
+        self.assertEqual(block_to_block_type(non_quote), block_type_paragraph)
+        
+    def test_ul(self):
+        ul_1 = "* this is an\n* unordered list\n- with both delimeters present"
+        ul_2 = "- this is an\n* unordered list\n- with both delimeters present"
+        self.assertEqual(block_to_block_type(ul_1), block_type_ul)
+        self.assertEqual(block_to_block_type(ul_2), block_type_ul)
+        
+    def test_ul_missing_character(self):
+        non_ul = "* This is an\n* unordered list\n with one line missing the leading '*' character"
+        self.assertEqual(block_to_block_type(non_ul), block_type_paragraph)
+        
+    def test_ol(self):
+        ol = "1. this\n2. is\n3. an ordered list"
+        self.assertEqual(block_to_block_type(ol), block_type_ol)
+        
+    def test_ol_wrong_order(self):
+        non_ol = "1. This\n3. is not\n2. an ordered list"
+        self.assertEqual(block_to_block_type(non_ol), block_type_paragraph)
+    
+    def test_ol_missing_character(self):
+        non_ol = "1. This\n2. is not\n. an ordered list"
+        self.assertEqual(block_to_block_type(non_ol), block_type_paragraph)
+        
+    def test_ol_missing_new_line(self):
+        non_ol = "1. This 2. is not\n3. an ordered list"
+        self.assertEqual(block_to_block_type(non_ol), block_type_paragraph)
+
+    def test_ordinary_paragraph(self):
+        paragraph = "This is just a paragraph"
+        self.assertEqual(block_to_block_type(paragraph), block_type_paragraph)
+        
